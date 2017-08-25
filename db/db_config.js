3319e7fc9bc1b8c9e1114076c4fcf4d81bd0ae67
Ben Childs <benjamin.t.childs@gmail.com>
2017-08-25 11:10:02 -0500

    travis config and jest + folder

--- db/db_config.js
+++ db/db_config.js
@@ -4,10 +4,17 @@ const { Pool, Client } = require('pg')
 // for connection information
 const pool = new Pool()
 
+<<<<<<< 876bafbbb86ed1506e5b6b7a7a90487b2a1e5297
 // pool.query('SELECT NOW()', (err, res) => {
 //   console.log(err, res)
 //   pool.end()
 // })
+=======
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+>>>>>>> travis config and jest + folder
 
 // // you can also use async/await
 // const res = await pool.query('SELECT NOW()')
@@ -19,6 +26,10 @@ const pool = new Pool()
 // await client.connect()
 
 // const res = await client.query('SELECT NOW()')
+<<<<<<< 876bafbbb86ed1506e5b6b7a7a90487b2a1e5297
 // await client.end()
 
 module.exports = pool;
+=======
+// await client.end()
+>>>>>>> travis config and jest + folder
